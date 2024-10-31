5e41f059eaeb8c0e90bde77087a987c12ffc620c
yonatan957 <yonatan22660@gmail.com>
2024-10-31 10:02:19 +0200

    connect to server

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
+import { useEffect, useState } from "react"
+import MissionDTO from "./DTO/missionDTO"
+import Board from "./components/Board";
 
-const BASE_URL="VITE_BASE_URL=https://reactexambackend.onrender.com/missions/8854554"
+const BASE_URL="https://reactexambackend.onrender.com/missions/8854554"
 
 function App() {
+  const [missions, setMissons] = useState<MissionDTO[]>([]);
+  useEffect(()=>{
+    (async()=>{
+      try {
+        const response = await fetch(BASE_URL)
+        setMissons(await response.json())
+      } catch (error:any) {
+        console.log(error.message)
+      }
+    })()
+  },[])
   return (
     <>
+      <Board missions={missions}/>
     </>
   )
 }
--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,6 +1,14 @@
+import MissionDTO from "../DTO/missionDTO"
+import Card from "./Card"
 
-export default function Board() {
+interface props{
+    missions:MissionDTO[]
+}
+export default function Board({missions}:props) {
   return (
-    <div>Board</div>
+    <div>
+        <h1 style={{color:"red"}}>Mission</h1>
+        {missions.map(mission=><Card mission={mission}/>)}
+    </div>
   )
 }
