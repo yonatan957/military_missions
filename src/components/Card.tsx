5e9ebd744e0ed85b07ebc28bb0bba2931b66f57f
yonatan957 <yonatan22660@gmail.com>
2024-10-31 09:46:28 +0200

    create card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,21 @@
+import MissionDTO from "../DTO/missionDTO"
 
-export default function Card() {
+interface props{
+    mission:MissionDTO
+}
+export default function Card({mission}:props) {
   return (
-    <div>Card</div>
+    <div>
+        <div>
+            <h1>Name: {mission.name}</h1>
+            <h3>Status: {mission.status}</h3>
+            <h3>Priority: {mission.priority}</h3>
+            <h3>Descriptio: {mission.description}</h3>
+        </div>
+        <div>
+            <button style={{backgroundColor:"red"}}>Delete</button>
+            <button style={{backgroundColor:"green"}}>Update</button>
+        </div>
+    </div>
   )
 }
